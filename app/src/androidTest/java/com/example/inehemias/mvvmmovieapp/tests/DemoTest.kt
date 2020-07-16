60de0bea4650860395ac217e552ef6c4aa985e39
Nehemias Miranda <mnehemias27@gmail.com>
2020-07-15 23:46:51 -0400

    Removed logs until Timber setup is complete

--- app/src/androidTest/java/com/example/inehemias/mvvmmovieapp/tests/DemoTest.kt
+++ app/src/androidTest/java/com/example/inehemias/mvvmmovieapp/tests/DemoTest.kt
@@ -3,7 +3,6 @@ package com.example.inehemias.mvvmmovieapp.tests
 import androidx.test.core.app.ActivityScenario
 import androidx.test.platform.app.InstrumentationRegistry
 import com.example.inehemias.mvvmmovieapp.testutils.AndroidTags
-import com.example.inehemias.mvvmmovieapp.testutils.BaseTestClass
 import com.example.inehemias.mvvmmovieapp.testutils.TestSetup
 import com.example.inehemias.mvvmmovieapp.testutils.TestTags
 import com.example.inehemias.mvvmmovieapp.ui.popular_movie.MainActivity
@@ -26,7 +25,6 @@ class DemoTest {
     @TestTags(runTags = ["Regression"])
     fun verifyThisTestWillRunForUS() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
-        Timber.d("Running test to verify that en_us was the param....")
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.inehemias.mvvmmovieapp", appContext.packageName)
         assertEquals("en_us", locale)
@@ -35,7 +33,6 @@ class DemoTest {
     @Test
     @TestTags(runTags = ["SMOKE_BR"])
     fun verifyThisTestWillRunForBrazil() {
-        Timber.d("Running test to verify that pt_br was the param....")
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.inehemias.mvvmmovieapp", appContext.packageName)
@@ -45,7 +42,6 @@ class DemoTest {
     @Test
     @TestTags(runTags = ["SMOKE_DE"])
     fun verifyThisTestWillRunForDE() {
-        Timber.d("Running test to verify that pt_br was the param....")
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.inehemias.mvvmmovieapp", appContext.packageName)
